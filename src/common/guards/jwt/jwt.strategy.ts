d0afc606f7dfeaff060d64191bf94ca5128e9d09
Rodrigo Espinosa <rodesar95@hotmail.com>
2025-11-05 14:23:00 -0600

    ADD: implement JWT authentication strategy and public decorator

---
+++ src/common/guards/jwt/jwt.strategy.ts
@@ -0,0 +1,46 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UserWithoutHash } from 'src/modules/users/interfaces/users.interface';
+import { JwtPayload } from 'src/modules/auth/interfaces/auth.interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private prisma: PrismaService,
+    configService: ConfigService,
+  ) {
+    const jwtSecret = configService.get<string>('JWT_SECRET');
+
+    if (!jwtSecret) {
+      throw new UnauthorizedException(
+        'JWT_SECRET environment variable is not defined.',
+      );
+    }
+
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: jwtSecret,
+    });
+  }
+
+  // This method is automatically called by Passport after the JWT token is verified.
+  // It ensures the user still exists and is active, and returns the user object
+  // (without the password hash) to be assigned to `req.user`.
+  async validate(payload: JwtPayload): Promise<UserWithoutHash> {
+    const user = await this.prisma.user.findUnique({
+      where: { id: payload.userId },
+      omit: { hash: true },
+    });
+
+    if (!user) throw new UnauthorizedException('User not found');
+
+    if (!user.is_active)
+      throw new UnauthorizedException('User account is disabled');
+
+    return user;
+  }
+}
