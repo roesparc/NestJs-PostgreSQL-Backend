88c72f6a9485096562c57ea759e961d398df71f7
Rodrigo Espinosa <rodesar95@hotmail.com>
2025-11-05 16:34:54 -0600

    UPDATE: enhance global validation pipe configuration and ensure CORS is enabled

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,16 @@ import { JwtAuthGuard } from './common/guards/jwt/jwt-auth.guard';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
   app.useGlobalGuards(new JwtAuthGuard(app.get(Reflector))); // enable auth guard for all endpoints by default
-  app.useGlobalPipes(new ValidationPipe()); // enable validations from DTO's
-  setupSwagger(app); // setup Swagger
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  ); // enable validations from DTO's
+  setupSwagger(app);
 
   await app.listen(process.env.PORT ?? 3000);
 }
