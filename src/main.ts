e08182193a09e18f34cfb1ce89d0a1fe13190ebf
Rodrigo Espinosa <rodesar95@hotmail.com>
2025-11-05 14:23:28 -0600

    ADD: enable global JWT authentication guard and validation pipes in main application

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import setupSwagger from './config/swagger.config';
+import { ValidationPipe } from '@nestjs/common';
+import { JwtAuthGuard } from './common/guards/jwt/jwt-auth.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalGuards(new JwtAuthGuard(app.get(Reflector))); // enable auth guard for all endpoints by default
+  app.useGlobalPipes(new ValidationPipe()); // enable validations from DTO's
   setupSwagger(app); // setup Swagger
 
   await app.listen(process.env.PORT ?? 3000);
