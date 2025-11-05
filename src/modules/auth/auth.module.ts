2c470ce012d14c1591cf1c2a8c4af38a8ee0cf61
Rodrigo Espinosa <rodesar95@hotmail.com>
2025-11-05 14:18:50 -0600

    ADD: AuthModule with JWT and Prisma integration

---
+++ src/modules/auth/auth.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { JwtStrategy } from '../../common/guards/jwt/jwt.strategy';
+import { AppLogger } from '../../common/logger/logger.service';
+import { PrismaModule } from '../../prisma/prisma.module';
+
+@Module({
+  imports: [
+    PrismaModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: configService.get('JWT_EXPIRES_IN') },
+      }),
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy, AppLogger],
+  exports: [AuthService],
+})
+export class AuthModule {}
