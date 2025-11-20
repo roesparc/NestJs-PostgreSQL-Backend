a332755f06cdadcf5b1cde44a40e2a4f9337b6c0
Rodrigo Espinosa <rodesar95@hotmail.com>
2025-11-19 18:25:31 -0600

    UPDATE: register global validation pipe and auth guard via DI instead of main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { RolesModule } from './modules/roles/roles.module';
 import { ProjectsModule } from './modules/projects/projects.module';
 import { PostsModule } from './modules/posts/posts.module';
+import { JwtAuthGuard } from './common/guards/jwt/jwt-auth.guard';
+import { APP_GUARD, APP_PIPE } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -14,6 +16,23 @@ import { PostsModule } from './modules/posts/posts.module';
     RolesModule,
     ProjectsModule,
     PostsModule,
+  ],
+  providers: [
+    {
+      // enable auth guard for all endpoints by default
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+    {
+      // enable validations from DTO's
+      provide: APP_PIPE,
+      useFactory: () =>
+        new ValidationPipe({
+          whitelist: true,
+          forbidNonWhitelisted: true,
+          transform: true,
+        }),
+    },
   ],
 })
 export class AppModule {}
